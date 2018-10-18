f1eaf207bf29e43d62dc21db2fdb06a415e06b40
paulvdbles <paulvdb08@hotmail.com>
2018-10-18 16:05:40 +0200

    Added complexity to data types

--- src/Model.hs
+++ src/Model.hs
@@ -19,44 +19,71 @@ initialState :: GameState
 initialState = GameState ShowNothing 0
 
 -- example stuff above
-
-data Creature = Creature
-  { speed   :: Int
-  , health  :: HealthPoints
-  , weapons :: [Weapon]
+data Spaceship = Spaceship
+  { speed    :: Int
+  , health   :: HealthPoints
+  , weapons  :: [Weapon]
+  , location :: Coordinates
   }
 
 data Player = Player
-  { score    :: Int
-  , creature :: Creature
+  { spaceship :: Spaceship
+  , score     :: ScorePoints
+  , combo     :: Int
+  , comboTime :: Seconds
   }
 
-newtype Enemy =
-  Enemy Creature
+data Enemy = Enemy
+  { bounty        :: ScorePoints
+  , evilSpaceship :: Spaceship
+  }
+
+data SpaceJunk = SpaceJunk
+  { bonusPoints :: ScorePoints
+  , junkHealth  :: HealthPoints
+  }
 
 data Item
-  = Weapon
+  = WeaponItem Weapon
   | PowerUp { bonusHealth :: HealthPoints }
 
 data Weapon
-  = Pistol { damage :: Damage }
-  | Laser { damage :: Damage }
-  | Bazooka { damage :: Damage }
+  = Pistol { damage :: DamagePoints }
+  | Laser { damage :: DamagePoints }
+  | Bazooka { damage     :: DamagePoints
+            , reloadTime :: Int }
 
-newtype Damage =
-  Damage Int
+newtype DamagePoints =
+  DamagePoints Int
 
 newtype HealthPoints =
   HealthPoints Int
 
-data Placeable = PlaceableCreature Creature | PlaceableItem Item
+newtype ScorePoints =
+  ScorePoints Int
+
+data Placeable
+  = PlaceableCreature Spaceship
+  | PlaceableItem Item
+  | PlaceableSpaceJunk SpaceJunk
 
 data Coordinates = Coordinates
-  { x    :: Int
-  , y    :: Int
+  { x :: Int
+  , y :: Int
   }
 
-newtype Spawn = Spawn (Placeable, Coordinates)
+newtype Spawn =
+  Spawn (Placeable, Coordinates)
 
-newtype Level = Level [Spawn]
+newtype Level =
+  Level [Spawn]
 
+data World = World
+  { player    :: Player
+  , enemies   :: [Enemy]
+  , spaceJunk :: [SpaceJunk]
+  , level     :: Level
+  }
+
+newtype Seconds =
+  Seconds Int
