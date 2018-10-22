e55caa6463d36d269fb37e604e072eaa435ffe34
paulvdbles <paulvdb08@hotmail.com>
2018-10-22 14:59:23 +0200

    Worked on rendering idea

--- src/View.hs
+++ src/View.hs
@@ -1,16 +1,25 @@
--- | This module defines how to turn
---   the game state into a picture
-
-{-# LANGUAGE LambdaCase #-}
-
 module View where
 
 import           Graphics.Gloss
-import           Graphics.Gloss.Juicy
+import           Graphics.Gloss.Interface.Pure.Game
 import           Model
-
--- should update view
 
 view :: World -> IO Picture
 view world = render initialPlayer
 
+-- We should use this but idk how yet
+
+drawWorld :: World -> [IO Picture]
+drawWorld world = [drawPlayer world, drawEnemies world, drawObstacles world, drawBullets world]
+
+drawPlayer :: World -> IO Picture
+drawPlayer world = render initialPlayer
+
+drawEnemies :: World -> IO Picture
+drawEnemies world = undefined
+
+drawObstacles :: World -> IO Picture
+drawObstacles world = undefined
+
+drawBullets :: World -> IO Picture
+drawBullets world = undefined
